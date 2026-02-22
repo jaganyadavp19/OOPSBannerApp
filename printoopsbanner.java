a31c0448c3460ad1559ad38fee44d877f64744a5
jaganyadavp19 <jaganyadavpullagura2006@gmail.com>
2026-02-22 21:49:54 +0530

    Update printoopsbanner.java

--- printoopsbanner.java
+++ printoopsbanner.java
@@ -1,11 +1,19 @@
-public class printoopsbanner{
-	public static void main(String[] args){
-		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **      ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + "  *****  ");
-		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "      ** ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + " **   ** ");
-		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  *****  ");
-	}
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", " ***** ", " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*      "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", " ***** ", " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*      ", "      *"));
+
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", "*      ", " ***** "));
+    }
 }
