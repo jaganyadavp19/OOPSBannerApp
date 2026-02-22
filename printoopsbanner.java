85091443174ec84c937dba54e2ce3a88408bff87
jaganyadavp19 <jaganyadavpullagura2006@gmail.com>
2026-02-22 21:50:56 +0530

    Update printoopsbanner.java

--- printoopsbanner.java
+++ printoopsbanner.java
@@ -1,11 +1,10 @@
-public class printoopsbanner{
-	public static void main(String[] args){
-		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **      ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + "  *****  ");
-		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "      ** ");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + " **   ** ");
-		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  *****  ");
-	}
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****     *****     ***** ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *    *****     ***** ");
+        System.out.println("*     *   *     *   *             *  ");
+        System.out.println(" *****     *****    *        *****  ");
+    }
 }
